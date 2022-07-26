5323c2aaa9c2fa46607e5b606aeead1ab2868ce6
Stephen Flores <sf314@icloud.com>
2022-07-26 10:07:45 -0500

    feat: Initialize firebase in the app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBg4Vvyk-JZvuigcCEikQYOG08k2tbZdwo",
+  authDomain: "wedding-site-106a8.firebaseapp.com",
+  projectId: "wedding-site-106a8",
+  storageBucket: "wedding-site-106a8.appspot.com",
+  messagingSenderId: "641390355622",
+  appId: "1:641390355622:web:f25524a2406d47787f8067",
+  measurementId: "G-2EKR1536ND"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 function App() {
   return (
     <div className="App">
