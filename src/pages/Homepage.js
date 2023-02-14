75fbf78b109f1d244870f42d57183e9b5a26fa71
Stephen Flores <sf314@icloud.com>
2023-02-14 15:51:53 -0600

    feat: Add initial page text

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -5,7 +5,21 @@ function Homepage() {
     logAnalytics("page_render_homepage")
     return (
         <div className="Homepage">
-            Welcome home! This is Homepage.js
+            <h2>
+                Mark Your Calendars!
+            </h2>
+            <p>
+                We will be celebrating our special day on Deecmber 15th, 2023!
+                We are super excited all of you to join us in our wedding 
+                at <a href='https://theashleycastle.com'>The Ashley Castle</a> in
+                Chandler, AZ.
+            </p>
+            <h2>
+                Venue Information and Directions
+            </h2>
+            <h2>
+                Nearby Hotels
+            </h2>
         </div>
     );
 }
