b0c4be91d4952a43d32fcd409389f92535627c32
Stephen Flores <sf314@icloud.com>
2023-02-14 14:56:18 -0600

    feat: Add firebase analytics for page rendering

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,5 +1,8 @@
+import './AboutUs.css'
+import logAnalytics from '../analytics/FirebaseAnalytics.js'
 
 function AboutUs() {
+    logAnalytics("page_render_about_us")
     return (
         <div>
             About us!
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,6 +1,8 @@
 import './Homepage.css'
+import logAnalytics from '../analytics/FirebaseAnalytics.js'
 
 function Homepage() {
+    logAnalytics("page_render_homepage")
     return (
         <div className="Homepage">
             Welcome home! This is Homepage.js
